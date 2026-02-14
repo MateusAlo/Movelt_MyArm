9caaa9baa122daa6285a431447196afdadaf8c0d
MateusAlo <mateusalonso@usp.br>
2026-02-14 14:46:41 -0300

    simple arm mover

--- src/simple_mover.cpp
+++ src/simple_mover.cpp
@@ -4,10 +4,51 @@
 
 
 int main(int argc, char** argv)
-{
+{   
+    // Initialization
     rclcpp::init(argc, argv);
-    auto node = rclcpp::Node::make_shared("simple_mover");
-    RCLCPP_INFO(node->get_logger(), "My log message %d", 4);
+    rclcpp::NodeOptions node_options;
+    node_options.automatically_declare_parameters_from_overrides(true);
+    auto node = rclcpp::Node::make_shared("simple_mover",node_options);
+
+    // Multithreaded executor
+    rclcpp::executors::MultiThreadedExecutor executor;
+    executor.add_node(node);
+    std::thread spin_thread([&executor]() { executor.spin(); });
+    
+    // Create the MoveGroupInterface
+    using moveit::planning_interface::MoveGroupInterface;
+    auto move_group_interface = MoveGroupInterface(node, "ur_manipulator");
+    
+    // Pose
+    auto const target_pose = []{
+    geometry_msgs::msg::Pose msg;
+    msg.orientation.w = 1.0;
+    msg.position.x = 0.28;
+    msg.position.y = -0.2;
+    msg.position.z = 0.5;
+    return msg;
+    }();
+
+    // Passing target pose to the move group interface
+    move_group_interface.setPoseTarget(target_pose);
+    
+    // Create a plan to that target pose
+    auto const [success, plan] = [&move_group_interface]{
+    moveit::planning_interface::MoveGroupInterface::Plan msg;
+    auto const ok = static_cast<bool>(move_group_interface.plan(msg));
+    return std::make_pair(ok, msg);
+    }();
+
+    // Execute the plan
+    if(success) {
+        move_group_interface.execute(plan);
+    } 
+    else {
+        RCLCPP_ERROR(node->get_logger(), "Planning failed!");
+    }
+    //RCLCPP_INFO(node->get_logger(), "Target pose set: %s", target_pose.position.x);
     rclcpp::shutdown();
+    spin_thread.join();
     return 0;
 }
